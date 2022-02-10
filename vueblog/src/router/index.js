86e905074f890c00a6b1870cf5035be3d990742a
Szabo <974381590@qq.com>
2022-02-10 18:13:47 +0800

    完成了前端的主页，栏目管理页面及相应功能

--- vueblog/src/router/index.js
+++ vueblog/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
+import CateMana from '@/components/CateMana'
 
 
 
@@ -13,6 +15,23 @@ export default new Router({
       name: '登录',
       hidden: true,
       component: Login
+    }, {
+      path: '/home',
+      name: '',
+      component: Home,
+      hidden: true
+    }, {
+      path: '/home',
+      component: Home,
+      name: '栏目管理',
+      children: [
+        {
+          path: '/cateMana',
+          iconCls: 'fa fa-reorder',
+          name: '栏目管理',
+          component: CateMana
+        }
+      ]
     }
   ]
 })
